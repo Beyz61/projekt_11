8fbf77814ee8adf40f93c7749a3f9f461a4809a7
beyzanur <beyzanur.o@icloud.com>
2024-09-13 13:33:21 +0200

    Classes User Registration und Anmeldung

---
+++ classes/user.dart
@@ -0,0 +1,38 @@
+import 'dart:io';
+
+// klasse
+class Registration {
+  String? nutzername;
+  String? password;
+  bool registrieren() {
+    print("Bitte wählen Sie einen Benutzernamen: ");
+    nutzername = stdin.readLineSync();
+    print("Bitte wählen Sie ein Passwort: ");
+    password = stdin.readLineSync();
+    if (nutzername != null &&
+        password != null &&
+        nutzername!.isNotEmpty &&
+        password!.isNotEmpty) {
+      print(
+          "Registrierung erfolgreich! Ihr Benutzername '$nutzername' und Passwort wurden gespeichert.");
+      return true;
+    } else {
+      print("Registrierung fehlgeschlagen. Bitte gültige Daten eingeben.");
+      return false;
+    }
+  }
+
+  bool anmelden() {
+    print("Bitte geben Sie Ihren Benutzernamen zur Anmeldung ein: ");
+    String? loginNutzername = stdin.readLineSync();
+    print("Bitte geben Sie Ihr Passwort ein: ");
+    String? loginPassword = stdin.readLineSync();
+    if (loginNutzername == nutzername && loginPassword == password) {
+      print("Anmeldung erfolgreich! Willkommen zurück, $nutzername.");
+      return true;
+    } else {
+      print("Falscher Benutzername oder Passwort.");
+      return false;
+    }
+  }
+}
